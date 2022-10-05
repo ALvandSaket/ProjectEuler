b5dde1ff9c0fde252cc13756945221fc6eac46ba
ALvandSaket <alvandsaket95@gmail.com>
2022-10-05 17:23:39 +0300

    changes made to the codes
    changes made to the codes

--- 010 - Summation of primes.py
+++ 010 - Summation of primes.py
@@ -1,8 +1,5 @@
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-# Find the sum of all the primes below two million.
 
 TARGET = 2000000
-
 
 def if_prime(x):
     for i in range(2, x):
@@ -10,12 +7,12 @@ def if_prime(x):
             return False
     return True
 
-
 nums = []
 total = 3
 
 for i in range(3, TARGET, 2):
     if if_prime(i):
         total += i
+        # print (total) 
 
 print(total)
